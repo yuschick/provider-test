eb796d1a6a9f8f37a2837ed827efe014c7d9f4c9
Daniel Yuschick <danyuschick@gmail.com>
2020-09-08 10:45:26 +0300

    move hook exports to main index

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { StoreProvider } from 'easy-peasy';
 
 import store from 'store';
+import { usePositiveStore, usePositiveActions } from 'store/hooks';
 
 const PositiveProvider: React.FC = ({ children }) => (
   <StoreProvider store={store}>{children}</StoreProvider>
 );
 
+export { usePositiveStore, usePositiveActions };
 export default PositiveProvider;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import { createStore } from 'easy-peasy';
-import { usePositiveStore, usePositiveActions } from './hooks';
 import { storeModel } from './model';
 
 const store = createStore(storeModel, {
   injections: {},
 });
 
-export { usePositiveStore, usePositiveActions };
 export default store;
