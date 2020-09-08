8eca456eeeb02edf4c403e8239d0018a0dceff63
Daniel Yuschick <danyuschick@gmail.com>
2020-09-08 13:52:24 +0300

    first react query test

--- src/stores/audiences/store.ts
+++ src/stores/audiences/store.ts
@@ -1,4 +1,5 @@
-import { Action, action } from 'easy-peasy';
+import { Action, action, Thunk, thunk } from 'easy-peasy';
+import { useQuery } from 'react-query';
 
 type Error = {
   fetchingAudiences: boolean;
@@ -8,8 +9,13 @@ export interface AudiencesModel {
   error: Error;
   loading: boolean;
 
+  audiences: any[];
+
   setLoading: Action<AudiencesModel, boolean>;
   setError: Action<AudiencesModel, Error>;
+  setAudiences: Action<AudiencesModel, any[]>;
+
+  fetchAudiences: Thunk<AudiencesModel>;
 }
 
 const storeModel: AudiencesModel = {
@@ -17,6 +23,7 @@ const storeModel: AudiencesModel = {
     fetchingAudiences: false,
   },
   loading: false,
+  audiences: [],
 
   setError: action((state, payload) => {
     state.error = payload;
@@ -24,6 +31,19 @@ const storeModel: AudiencesModel = {
 
   setLoading: action((state, payload) => {
     state.loading = payload;
+  }),
+
+  setAudiences: action((state, payload) => {
+    state.audiences = payload;
+  }),
+
+  fetchAudiences: thunk(async (actions, payload) => {
+    const { isLoading, error, data } = useQuery(
+      'repoData',
+      async () =>
+        await fetch('https://api.github.com/repos/tannerlinsley/react-query')
+    );
+    return { isLoading, error, data };
   }),
 };
 
