bebcecea98e18d50939a72c8eb63f02b24e59fa8
Daniel Yuschick <danyuschick@gmail.com>
2020-09-08 13:33:30 +0300

    removed context directory

--- src/context/hooks.ts
+++
@@ -1,8 +0,0 @@
-import { createTypedHooks } from 'easy-peasy';
-
-import { StoreModel } from './model';
-
-const typedHooks = createTypedHooks<StoreModel>();
-
-export const usePositiveActions = typedHooks.useStoreActions;
-export const usePositiveStore = typedHooks.useStoreState;
--- src/context/index.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'easy-peasy';
-import { storeModel } from './model';
-
-const store = createStore(storeModel, {
-  injections: {},
-});
-
-export default store;
