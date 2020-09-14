c1bc619b2bd51df045c114531977745760a21f0f
Daniel Yuschick <danyuschick@gmail.com>
2020-09-14 14:55:20 +0300

    whatever

--- src/hooks/audiences/useAudiences.ts
+++ src/hooks/audiences/useAudiences.ts
@@ -1,36 +1,23 @@
 import { useQuery } from 'react-query';
 
-import usePositiveStore from '../store';
 import { UseAudiences } from '../../types/audiences';
 
 function useAudiences(): UseAudiences {
-  const { target, language } = usePositiveStore();
-  console.log(target, language);
-
-  const cleanAudiences = (data: any) => console.log(data);
-
   const audienceQuery = useQuery(
     'fetchAudiences',
-    () => {
-      console.log('in query');
+    () =>
       fetch(
         'https://api.github.com/repos/tannerlinsley/react-query'
-      ).then(res => res.json());
-    },
+      ).then(res => res.json()),
     {
       enabled: false,
     }
   );
 
-  const fetchAudiences = () => {
-    const { refetch } = audienceQuery;
-    console.log('in refetch');
-    refetch();
+  return {
+    fetchAudiences: audienceQuery.refetch,
+    audiences: audienceQuery.data,
   };
-
-  const audiences = cleanAudiences(audienceQuery.data);
-
-  return { fetchAudiences, audiences };
 }
 
 export default useAudiences;
